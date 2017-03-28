2e71cdf5722a8edc21f7935bb3dcacee18ac9487
Shane Bodimer <shanebodimer@Shanes-MBP.attlocal.net>
2017-03-28 18:17:52 -0500

    Converted index.php to index.html

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,18 +1,18 @@
 
 $(window).scroll(function() {
-  
-  //Get scroll position    
+
+  //Get scroll position
   var scroll = $(window).scrollTop();
 
   //If scrolling
-  if (scroll >= 1) 
+  if (scroll >= 1)
   {
     $(".nav-mobile-item").removeClass("nav-color-top");
     $(".nav-mobile-item").addClass("nav-color-scrolling");
     $(".nav-mobile").removeClass("nav-color-top");
     $(".nav-mobile").addClass("nav-color-scrolling");
-    //$(".nav-activate").removeClass("nav-color-top");
-    //$(".nav-activate").addClass("nav-color-scrolling");
+    $(".nav-activate").removeClass("nav-color-top");
+    $(".nav-activate").addClass("nav-color-scrolling");
     $(".nav-item").removeClass("nav-color-top");
     $(".nav-item").addClass("nav-color-scrolling");
     $(".nav-logo-words").removeClass("nav-color-top");
@@ -22,16 +22,16 @@ $(window).scroll(function() {
     $(".nav-item").removeClass("nav-item-hover-top");
     $(".nav-item").addClass("nav-item-hover-scroll");
   }
-  
+
   //If at top
-  else 
+  else
   {
     $(".nav-mobile-item").addClass("nav-color-top");
     $(".nav-mobile-item").removeClass("nav-color-scrolling");
     $(".nav-mobile").addClass("nav-color-top");
     $(".nav-mobile").removeClass("nav-color-scrolling");
-    //$(".nav-activate").addClass("nav-color-top");
-    //$(".nav-activate").removeClass("nav-color-scrolling");
+    $(".nav-activate").addClass("nav-color-top");
+    $(".nav-activate").removeClass("nav-color-scrolling");
     $(".nav-item").addClass("nav-color-top");
     $(".nav-item").removeClass("nav-color-scrolling");
     $(".nav-logo-words").addClass("nav-color-top");
@@ -39,6 +39,6 @@ $(window).scroll(function() {
     $(".nav-full").addClass("nav-full-top");
     $(".nav-full").removeClass("nav-full-scrolling");
     $(".nav-item").addClass("nav-item-hover-top");
-    $(".nav-item").removeClass("nav-item-hover-scroll");  
+    $(".nav-item").removeClass("nav-item-hover-scroll");
   }
 });
